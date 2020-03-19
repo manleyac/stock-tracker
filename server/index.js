e31bf25c6d31f8f306596682acc55f131ae8c83b
Andrew <8879789+manleyac@users.noreply.github.com>
2020-03-19 17:11:49 -0400

    added validation to user post route

--- server/index.js
+++ server/index.js
@@ -7,10 +7,14 @@ const PORT = process.env.PORT || 5000;
 //DB connection
 connectDB();
 
+//middleware
+app.use(express.json({ extended: false}));
+
 //routes
 app.get("/", (req, res) => res.send("API Running"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/portfolio", require("./routes/api/portfolio.js"));
 
 //start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+const 
+
 const router = express.Router();
 
+const User = require("../../models/User");
 
-//@route  GET api/users
-//@desc  Test route
-//@access  Public
-router.get("/",(req,res) => res.send("User Route"));
+//@route  POST api/users
+//@desc   register route
+//@access Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password must meet requirements").isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    //check if user already exists
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User Already Exists" }] });
+      }
+      user = new User({ name, email, password });
+
+    } catch (err) {
+      console.error(error);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
